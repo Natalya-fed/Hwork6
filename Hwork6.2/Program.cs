644abdf2eb4613da6897313e17a61fc033d3b72c
Наталья Фёдорова <natalyafedorova5@gmail.com>
2021-10-24 23:05:36 +0300

    Добавьте файлы проекта.

---
+++ Hwork6.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hwork6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string pa = Console.ReadLine();
+            string[] s = pa.Split(' ');
+            string pa1 = "";
+            string ap = "";
+            foreach (string a in s)
+            {
+                pa1 += a.ToLower();
+            }
+            for (int i = pa1.Length - 1; i >= 0; i--)//перебираем с конца
+            {
+                ap += pa1[i].ToString();
+            }
+            if (pa1 == ap)
+            {
+                Console.WriteLine($"Строка палиндрома:\n{pa1}\n{ap}");
+            }
+            else
+            {
+                Console.WriteLine($"Строка не палиндрома:\n{pa1}\n{ap}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hwork6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hwork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string p = Console.ReadLine();
+            string[] s = p.Split(' ');
+            int max = 0;
+            string maxWord = "";
+            foreach (string a in s)
+            {
+                int word = a.Length;
+                if (max < word)
+                {
+                    max = word;
+                    maxWord = a;
+                }
+                else
+                {
+                    if (max == word)
+                    {
+                        max = word;
+                        maxWord += ", " + a;
+                    }
+                }
+            }
+            Console.WriteLine($"Самые длинные слова:\n{maxWord}");
+            Console.ReadKey();
+        }
+    }
+}
